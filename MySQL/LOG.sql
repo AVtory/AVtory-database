d64d0793abbbe2bfc0689d6f858f4bd927bec959
jasonsneddon <sneddon.jason@yahoo.com>
2017-12-10 11:35:05 -0800

    Rearranged foreign keys for clarity

--- MySQL/LOG.sql
+++ MySQL/LOG.sql
@@ -8,10 +8,10 @@
 CREATE TABLE `LOG` 
 (
   `Log_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Employee_ID` int(11) NOT NULL,
+  `Item_ID` int(11) NOT NULL,  
   `Log_Date` datetime NOT NULL,
   `Event` enum('checkin','checkout') NOT NULL DEFAULT 'checkin',
-  `Employee_ID` int(11) NOT NULL,
-  `Item_ID` int(11) NOT NULL,
   `Comment` varchar(150) DEFAULT NULL,
   
   PRIMARY KEY (`Log_ID`),
