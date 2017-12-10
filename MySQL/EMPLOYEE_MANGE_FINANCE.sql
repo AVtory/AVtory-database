7e0c7fee6d625f2a4aa545e96c5fab5f5de88ca6
jasonsneddon <sneddon.jason@yahoo.com>
2017-12-10 11:13:39 -0800

    Updated Syntax

--- MySQL/EMPLOYEE_MANGE_FINANCE.sql
+++ MySQL/EMPLOYEE_MANGE_FINANCE.sql
@@ -1,24 +1,20 @@
 /*
  * Author  - Jason Sneddon
- * Date    - 25 October 2017
+ * Date    - 10 December 2017
  * Course  - CS 441: Software Engineering
  * Purpose - AVtory Database: Illustrates the Many-to-Many 
  *           Relationship between EMPLOYEE & FINANCE
  */
 
-CREATE TABLE EMPLOYEE_MANAGE_FINANCE
+CREATE TABLE `EMPLOYEE_MANAGE_FINANCE` 
 (
-	/* Both are Primary and Foreign Keys */
-	Employee_ID INTEGER(11) NOT NULL, 
-	Finance_ID INTEGER(11) NOT NULL,
-	
-	CONSTRAINT Employee_Manage_Finance_PK PRIMARY KEY(Employee_ID, Finance_ID)
-	
-	/* === Foreign Keys === */
-	/*
-	CONSTRAINT Employee_Manage_Finance_Employee_FK FOREIGN KEY(Employee_ID)
-		REFERENCES EMPLOYEE(Employee_ID) ON DELETE CASCADE, 
-	CONSTRAINT Employee_Manage_Finance_Finance_FK FOREIGN KEY(Finance_ID)
-		REFERENCES FINANCE(Finance_ID) ON DELETE CASCADE,
-	*/
-);
+  `Employee_ID` int(11) NOT NULL,
+  `Finance_ID` int(11) NOT NULL,
+  
+  PRIMARY KEY (`Employee_ID`,`Finance_ID`),
+  KEY `Employee_Manage_Finance_Finance_FK` (`Finance_ID`),
+  CONSTRAINT `Employee_Manage_Finance_Employee_FK` FOREIGN KEY (`Employee_ID`) 
+  	REFERENCES `EMPLOYEE` (`Employee_ID`) ON DELETE CASCADE,
+  CONSTRAINT `Employee_Manage_Finance_Finance_FK` FOREIGN KEY (`Finance_ID`) 
+  	REFERENCES `FINANCE` (`Finance_ID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
